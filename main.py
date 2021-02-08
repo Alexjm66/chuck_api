52b1ffecbde69822252337a35d2a20fb77fa4fc0
Alexjm66 <alexjmaart665@gmail.com>
2021-02-08 14:54:32 +0200

    main

--- main.py
+++ main.py
@@ -8,6 +8,6 @@ def get_chuck_norris_jokes():
 
     api_url = "https://api.chucknorris.io/jokes/random"
     response = requests.get(api_url).json()
-    images="<img src="+response ['icon_url'] + "alt='Chucks image'>"
+    images="<img src="+response ['icon_url'] + " alt='Chucks image'>"
     return "<strong>Random joke from chuck norris: </strong>" + response['value'] + images
 
